dabe4c07ff878e37a1300bfd2d1d411d33b8c3ba
mohamedsamibouanani-cmyk <mohamedsami.bouanani@uit.ac.ma>
2026-08-15 21:16:47 +0100

    Activer les signaux apprentissage ML

--- supervision/apps.py
+++ supervision/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class SupervisionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'supervision'
+
+    def ready(self):
+        # Enregistre le déclenchement du réentraînement après création d'un
+        # exemple d'apprentissage validé par le superviseur.
+        from . import signals  # noqa: F401
